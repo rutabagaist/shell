59e98b898299fb4ed4fd6dd3c78e0b64c706911d
rutabagaist <37372125+rutabagaist@users.noreply.github.com>
2025-10-21 17:39:44 -0400

    Moving the whole rawdata directory instead of just contents.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,7 +32,7 @@ unzip -q rawdata.zip
 mkdir -p ./data/raw
 
 # 2. Move the ./rawdata directory to ./data/raw
-mv ./rawdata/* ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
 ls -lh ./data/raw
